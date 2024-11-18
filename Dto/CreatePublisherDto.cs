6c65f437852a9d7a105a07d2f446bd0198531c2b
Ruchika Deore <ruchikadeore163@gmail.com>
2024-11-18 16:56:06 +0530

    CreatePublisherDto

---
+++ Dto/CreatePublisherDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store_Portal.Dto
+{
+    public class CreatePublisherDto
+    {
+        [Required]
+        public string? PubName { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+
+        [Required]
+        public string? State { get; set; }
+
+        [Required]
+        public string? Country { get; set; }
+    }
+}
