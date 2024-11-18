fdbda06aa05534a1925538f1bede8c435056058b
Irfana Syed <irfanairfana974@gmail.com>
2024-11-18 17:10:23 +0530

    Update Author dto

--- Dto/CreateAuthorDto.cs
+++ Dto/CreateAuthorDto.cs
@@ -1,6 +1,10 @@
-﻿namespace Book_Store_Portal.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store_Portal.Dto
 {
     public class CreateAuthorDto
     {
+        [Required]
+        public string Author_state {  get; set; }
     }
 }
